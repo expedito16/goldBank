4b29f679475f34f881b03e2b776bc5f43fac177a
Expedito <expeditopaivaneto@gmail.com>
2021-11-05 17:36:14 -0300

    criacao do componente de criar transação e e aplicando no app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CriarTransacaoComponent } from './view/criar-transacao/criar-transacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CriarTransacaoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view/criar-transacao/criar-transacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-transacao',
+  templateUrl: './criar-transacao.component.html',
+  styleUrls: ['./criar-transacao.component.scss']
+})
+export class CriarTransacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
